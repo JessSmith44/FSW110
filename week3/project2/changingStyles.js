b99a229aa131aaf08013ec23831083c230698683
unknown <jessica.k.smith@bryanuniversity.edu>
2021-12-09 15:29:14 -0700

    issues resolved

--- week3/project2/changingStyles.js
+++ week3/project2/changingStyles.js
@@ -1,31 +1,18 @@
-var headings = [
-    "Good morning/afternoon/evening!", 
-    "The holidays are coming", 
-    "The cold is setting in", 
-    "We don't have snow here yet", 
-    "Its going to be a great season", 
-];
-var h2 = document.createElement("h2");
-
-var learning = document.getElementById("headings"); 
-
-for (var i = 0; i < headings.length; i++){
-    var titles = document.createElement("h2");
-    titles.textContent = headings[i];
-    titles.style.color = "cornflowerblue";
-    titles.style.fontFamily = "sans serif";
-    titles.style.fontSize = "20px";
-    h2.appendChild(titles);
+for (var i = 0; i < 5; i++){
+    var headings = document.createElement("h2");
+    headings.textContent = "Good day to you all";    
+    document.body.appendChild(headings);
 }  
 
-document.body.prepend(h2);
-document.addEventListener('DOMContentLoaded', () => {
-    headings("h2");
-   });
+var titles = document.querySelectorAll("h2");
 
-   document.getElementById("border").classList.add("h2");
+for (let i = 0; i < titles.length; i++){ 
+    titles[i].style.fontFamily = "sans serif";
+    titles[i].style.fontSize = "20px";
+    titles[i].style.fontWeight = "lighter";  
+    titles[i].style.color = "cornflowerblue";
+}
 
-   function addClass() {
-    const element = document.getElementById("id1");
-    // passing multiple classes separated by comma
-    element.classList.add("color", "border", "others");
+for (let i = 0; i < titles.length; i++){    
+    titles[i].classList.add("border");
+}
