a3ac83d7c82721f66cd378b94d7f7960a5103bae
unknown <jessica.k.smith@bryanuniversity.edu>
2021-12-15 20:15:48 -0700

    colorful

--- week4/eventListeners.js
+++ week4/eventListeners.js
@@ -1,55 +1,62 @@
 
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
-addEventListener('resize', () => {
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
-  });
-c.strokeStyle = "white";
-c.fillStyle = "black";
-c.rect(100, 20, 200, 200);
-c.stroke();
-c.fill();
+const square = document.createElement("div");
+square.style.width = "250px";
+square.style.height = "250px";
+square.style.backgroundColor = "white";
+square.style.margin = "0 auto";
+square.style.marginTop = "20px";
+document.body.appendChild(square);
 
-function docReady(fn) {
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
-} 
-function green(){
-  green.fillStyle = "green";
-  green.target.style.color = "green";
-  green.addEventListener("mouseover", green);
-  return green;
-  }
-  
-  function yellow(){
-  yellow.fillStyle = "yellow";
-  yellow.event.target.style.color = "yellow";
-  yellow.addEventListener("click", yellow);
-  return yellow;
-  } 
+for (let i = 0; i < 20; i++) {
+    const h1 = document.createElement("h1");
+    h1.textContent = "Color box";
+    document.body.appendChild(h1);
+}
 
-  function blue(){
-  blue.fillStyle = "blue";
-  blue.event.target.style.color = "blue";
-  blue.addEventListener("click", blue);
-  return blue;
-  }
+window.addEventListener("load", function() {
+    square.style.backgroundColor = "black";
+});
 
-  function red(){
-    red.fillStyle = "red";
-    red.event.target.style.color = "red";
-    red.addEventListener("click", red);
-    return red;
+square.addEventListener("mouseover", function() {
+    square.style.backgroundColor = "green";
+});
+
+square.addEventListener("mousedown", function() {
+    square.style.backgroundColor = "yellow";
+});
+
+square.addEventListener("mouseup", function() {
+    square.style.backgroundColor = "blue";
+});
+
+square.addEventListener("dblclick", function() {
+    square.style.backgroundColor = "red";
+});
+
+window.addEventListener("scroll", function() {
+    square.style.backgroundColor = "purple";
+})
+
+function setSquareBackgroundColor(color) {
+    square.style.backgroundColor = color;
+};
+
+window.addEventListener("keydown", function(event) {
+    switch (event.which) {
+        case 82:
+            setSquareBackgroundColor("red");
+            break;
+        case 71:
+            setSquareBackgroundColor("green");
+            break;
+        case 66:
+            setSquareBackgroundColor("blue");
+            break;
+        case 89:
+            setSquareBackgroundColor("yellow");
+            break;
+        case 80:
+            setSquareBackgroundColor("purple");
+            break;
     }
-    
-    function purple(){
-    purple.fillStyle = "purple";
-    purple.event.target.style.color = "purple";
-    purple.addEventListener("click", purple);
-    return purple;
-    }
-    
+});
