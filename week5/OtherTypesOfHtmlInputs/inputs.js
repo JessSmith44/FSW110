9da44122f78df39ba559632c35eae2c15728af82
unknown <jessica.k.smith@bryanuniversity.edu>
2021-12-27 17:46:29 -0700

    finished with styles

--- week5/OtherTypesOfHtmlInputs/inputs.js
+++ week5/OtherTypesOfHtmlInputs/inputs.js
@@ -1,6 +1,17 @@
-const form = document.form
+var form = document.getElementById("form");
+var selectMenu = document.getElementById("options");
 
-form.addEventListener("submit", (event) =>{
-    event.preventDefault();
-    alert("hello");
-})
+form.addEventListener("submit", function() {
+    var checkbox = document.querySelectorAll('input[type = "checkbox"]:checked');
+    var stuffedAnimals = [];
+    for (var i = 0; i < checkbox.length; i++) {
+        stuffedAnimals.push(" " + checkbox[i].value);
+
+    }
+    alert(` 
+    First Name: ${form.firstnamebox.value}
+    Last Name: ${form.lastnamebox.value}
+    Age: ${form.agebox.value}
+    How likely to reccomend: ${selectMenu.options[selectMenu.selectedIndex].value}
+    Your choice of Stuffed Animals: ${stuffedAnimals}`);
+});
