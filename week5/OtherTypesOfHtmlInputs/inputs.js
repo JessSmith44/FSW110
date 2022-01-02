ef2379a70918ca69f931703b0788de2916152c7d
JessSmith44 <91652487+JessSmith44@users.noreply.github.com>
2022-01-02 11:44:36 -0700

    Update inputs.js

--- week5/OtherTypesOfHtmlInputs/inputs.js
+++ week5/OtherTypesOfHtmlInputs/inputs.js
@@ -3,6 +3,7 @@ var selectMenu = document.getElementById("options");
 
 form.addEventListener("submit", function() {
     var checkbox = document.querySelectorAll('input[type = "checkbox"]:checked');
+    var regular = document.querySelectorAll('input[type = "radio"]:checked').value;
     var stuffedAnimals = [];
     for (var i = 0; i < checkbox.length; i++) {
         stuffedAnimals.push(" " + checkbox[i].value);
@@ -12,6 +13,7 @@ form.addEventListener("submit", function() {
     First Name: ${form.firstnamebox.value}
     Last Name: ${form.lastnamebox.value}
     Age: ${form.agebox.value}
+    Have you shopped with us before: ${form.decision.value}
     How likely to reccomend: ${selectMenu.options[selectMenu.selectedIndex].value}
     Your choice of Stuffed Animals: ${stuffedAnimals}`);
 });
